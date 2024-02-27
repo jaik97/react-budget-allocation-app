5847a11d45c97e7dfeacf1ae01dfaab0ee24287f
Jainendra <jainendra.kumar.jain@gmail.com>
2024-02-27 11:01:35 -0500

    Updating the App.js to club the components

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
-import ExpenseTotal from './components/ExpenseTotal';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
+                    
                     <div className='row mt-3'>
                         {
                         // Budget component
@@ -25,7 +30,9 @@ const App = () => {
 
                         {
                             //Remaining Component
-                            <div className='col-sm'><Remaining /></div>
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
@@ -36,17 +43,25 @@ const App = () => {
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            //ExpenseList
+                            <div className='col-sm'>
+                                <ExpenseList />
+                            </div>
                         }         
 
                         {
                             /* Add ExpenseItem component here */
+                            <div className='col-sm'>
+                                <ExpenseItem />
+                            </div>
                         }        
 
                         {
                             /* Add AllocationForm component here under */
-                        }        
-
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>
+                        }
                 </div>
             </div>
         </AppProvider>
