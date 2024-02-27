67c83275d06ac4204919d3d392e2f115ea3a2120
jaik97 <jainendra.kumar.jain@gmail.com>
2024-02-27 22:37:34 +0530

    Final Changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,15 +3,15 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const [NewBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+    <div className='alert alert-secondary'>
+        <span>Budget: £</span>
+        <input type="number" step="10" value={NewBudget} onChange={handleBudgetChange}></input>
+    </div>
     );
 };
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react';
-import {TiDelete} from 'react-icons/ti';
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const {dispatch} = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -25,12 +25,12 @@ const ExpenseItem = (props) => {
 
     }
 
-    return(
+    return (
         <tr>
-            <td>{props.name}</td>
-            <td>£{props.cost}</td>
-            <td><button onClick={event => increaseAllocation(props.name)}>+</button></td>
-            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}</TiDelete></td>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
