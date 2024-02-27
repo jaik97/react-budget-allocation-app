e5d2a8100c1a507f4d7264113d02e2248fa9c2ef
jaik97 <jainendra.kumar.jain@gmail.com>
2024-02-27 22:55:48 +0530

    Fixed Budget Updation in redux store

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,16 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+// import { useDispatch } from 'react-redux';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [NewBudget, setNewBudget] = useState(budget);
+    // const dispatch = useDispatch();
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        })
     }
+    // const handleBudgetChange = (event) => {
+    //     setNewBudget(event.target.value);
+    // }
+
     return (
     <div className='alert alert-secondary'>
         <span>Budget: £</span>
-        <input type="number" step="10" value={NewBudget} onChange={handleBudgetChange}></input>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
     </div>
     );
 };
