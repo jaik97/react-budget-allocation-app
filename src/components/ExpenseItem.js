d9046073ad67e85ba0c712ae954221668a882f88
jaik97 <jainendra.kumar.jain@gmail.com>
2024-03-02 00:46:47 +0530

    Allocation decrease button added with functionality

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiMinus, TiPlus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { currency, dispatch } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -25,11 +25,24 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
+    
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><TiPlus size='1.5em' style={{color: 'white', background: 'green', borderRadius: '50%'}} onClick={event=> increaseAllocation(props.name)}></TiPlus></td>
+        <td><TiMinus size='1.5em' style={{color: 'white', background: 'red', borderRadius: '50%'}} onClick={event=> decreaseAllocation(props.name)}></TiMinus></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
